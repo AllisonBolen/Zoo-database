762c0ce5bdebf097dfe703e34ca6adae6f75deba
Allison Bolen <bolena@mail.gvsu.edu>
2017-12-03 22:38:42 -0500

    fixes

--- Project.sql
+++ Project.sql
@@ -1,11 +1,14 @@
 SPOOL ZooDB.out
 SET ECHO ON
+SET WRAP OFF
+SET LINESIZE 30000
+SET TRIMSPOOL ON
 --
 -- CIS 353 - Database Project: Zoo Database
 --
 -- Authors:
 --
--- Allison Bolen2
+-- Allison Bolen
 -- Andrew Olesak
 -- Jake Walton
 -- Kasey Stowell
@@ -30,22 +33,26 @@ DROP TABLE worksat CASCADE CONSTRAINTS;
 --
 CREATE TABLE zooemployees
 (
-empssn		INTEGER			PRIMARY KEY,
-firstname	VARCHAR2(15)	NOT NULL,
-lastname	VARCHAR2(15)	NOT NULL,
-position	VARCHAR2(15)	NOT NULL,
-eaddress	VARCHAR2(60)	NOT NULL,
-esalary		INTEGER			NOT NULL,
-ebdate		DATE			NOT NULL,
-egender		CHAR			NOT NULL,
-superssn	INTEGER,
-exhibitname	VARCHAR2(20)	NOT NULL,
+empssn        INTEGER,
+firstname    VARCHAR2(10)    NOT NULL,
+lastname    VARCHAR2(10)    NOT NULL,
+position    VARCHAR2(10)    NOT NULL,
+eaddress    VARCHAR2(15)    NOT NULL,
+esalary        INTEGER            NOT NULL,
+ebdate        DATE            NOT NULL,
+egender        CHAR            NOT NULL,
+superssn    INTEGER,
+exhibitname    VARCHAR2(16)    NOT NULL,
+/*
+PK1: The primary key of zoo employee is empssn.
+*/
+CONSTRAINT PK1 PRIMARY KEY (empssn),
 /*
 ZC1: The position is one of: Supervisor, Cashier, Barista, Cook, 
 Janitor, Caretaker, or Vet.
 */
 CONSTRAINT ZC1 CHECK (position IN ('Supervisor', 'Cashier', 'Barista', 
-					'Cook', 'Janitor', 'Caretaker', 'Vet')),
+                    'Cook', 'Janitor', 'Caretaker', 'Vet')),
 /*
 ZC2: The employee gender is one of: M or F.
 */
@@ -59,35 +66,35 @@ CONSTRAINT ZC3 CHECK (NOT (position = 'Supervisor' AND esalary < 40000))
 --
 CREATE TABLE exhibit
 (
-exhibitname	VARCHAR2(20)	PRIMARY KEY,	
-climate		VARCHAR2(20)	NOT NULL,
-managerssn	INTEGER,
+exhibitname    VARCHAR2(16)    PRIMARY KEY,    
+climate        VARCHAR2(9)    NOT NULL,
+managerssn    INTEGER,
 /*
 ZC4: The climate is one of: Temperate, Polar, Tropical, Arid, 
-or Mediterranean.
+or Coastal.
 */
 CONSTRAINT ZC4 CHECK (climate IN ('Temperate', 'Polar', 'Tropical', 
-					'Arid', 'Mediterranean'))
+                    'Arid', 'Coastal'))
 );
 --
 --
 CREATE TABLE shop
 (
-shopid		INTEGER			PRIMARY KEY,
-sname		VARCHAR2(30)	NOT NULL,
-exhibitname	VARCHAR2(20)	NOT NULL
+shopid        INTEGER            PRIMARY KEY,
+sname        VARCHAR2(30)    NOT NULL,
+exhibitname    VARCHAR2(16)    NOT NULL
 );
 --
 --
 CREATE TABLE animal
 (
-aid			INTEGER			PRIMARY KEY,
-species		VARCHAR2(30)	NOT NULL,
-age			INTEGER			NOT NULL,
-agender		CHAR			NOT NULL,
-empssn		INTEGER			NOT	NULL,
-tendtime	INTEGER			NOT NULL,
-exhibitname	VARCHAR2(20)	NOT NULL,
+aid            INTEGER            PRIMARY KEY,
+species        VARCHAR2(16)    NOT NULL,
+age            INTEGER            NOT NULL,
+agender        CHAR            NOT NULL,
+empssn        INTEGER            NOT    NULL,
+tendtime    INTEGER            NOT NULL,
+exhibitname    VARCHAR2(16)    NOT NULL,
 /*
 ZC5: The animal gender is one of: M or F.
 */
@@ -100,23 +107,23 @@ CONSTRAINT ZC6 CHECK (tendtime > 559 AND tendtime < 1900)
 --
 CREATE TABLE event
 (
-exhibitname	VARCHAR2(20),
-evdate		DATE,
-type		VARCHAR2(20)	NOT NULL,
-PRIMARY KEY(exhibitname, evdate)	
+exhibitname    VARCHAR2(16),
+evdate        DATE,
+type        VARCHAR2(20)    NOT NULL,
+PRIMARY KEY(exhibitname, evdate)    
 );
 --
 CREATE TABLE shopproducts
 (
-shopid		INTEGER,
-productname	VARCHAR2(35),
-PRIMARY KEY(shopid, productname)		
+shopid        INTEGER,
+productname    VARCHAR2(35),
+PRIMARY KEY(shopid, productname)        
 );
 --
 CREATE TABLE worksat
 (
-empssn		INTEGER,
-shopid		INTEGER,
+empssn        INTEGER,
+shopid        INTEGER,
 PRIMARY KEY(empssn, shopid)
 );
 --
@@ -249,20 +256,19 @@ SET FEEDBACK OFF
 alter session set NLS_DATE_FORMAT = 'YYYY-MM-DD';
 --
 -- Supervisors
-
-insert into zooemployees values (130423454, 'Ronnie', 'Alvarado', 'Supervisor', '4960 Farland Street', 72000, '1970-04-15', 'M', NULL, 'Bugs');
-insert into zooemployees values (635052791, 'Patricia', 'Scott', 'Supervisor', '1743 Cinnamon Lane', 60000, '1971-09-27', 'F', 130423454, 'Tiger Realm');
-insert into zooemployees values (543145276, 'Brenda', 'Myers', 'Supervisor', '2338 Skinner Hollow Road', 61000, '1962-05-31', 'F', 130423454, 'Shores Aquarium');
-insert into zooemployees values (187225055, 'Michael', 'Tejada', 'Supervisor', '531 Stoney Lonesome Road', 62000, '1975-09-01', 'M', 130423454, 'Pelican Pier');
-insert into zooemployees values (397981967, 'David', 'Gullett', 'Supervisor', '4796 Trouser Leg Road', 63000, '1979-01-29', 'M', 130423454, 'Tropic Treasures');
-insert into zooemployees values (405249752, 'Reginald', 'Phillips', 'Supervisor', '1984 Straford Park', 64000, '1985-08-22', 'M', 130423454, 'Wild Way Trail');
-insert into zooemployees values (198204924, 'Jack', 'Arnold', 'Supervisor', '2385 Pride Avenue', 65000, '1983-05-10', 'M', 130423454, 'Petting Zoo');
-insert into zooemployees values (306369902, 'Robert', 'Bradley', 'Supervisor', '1524 Neville Street', 66000, '1974-01-24', 'M', 130423454, 'Africa');
-insert into zooemployees values (594494079, 'Sheila', 'Lane', 'Supervisor', '4387 Badger Pond Lane', 67000, '1975-11-13', 'F', 130423454, 'North America');
-insert into zooemployees values (660054663, 'Jennifer', 'Atencio', 'Supervisor', '4035 Wood Street', 68000, '1989-12-05', 'F', 130423454, 'South America');
-insert into zooemployees values (170725571, 'Allie', 'Owens', 'Supervisor', '3475 Lost Creek Road', 69000, '1985-12-14', 'F', 130423454, 'Frogs');
+insert into zooemployees values (130423454, 'Ronnie', 'Alvarado', 'Supervisor', '496 Farland St', 72000, '1970-04-15', 'M', NULL, 'Bugs');
+insert into zooemployees values (635052791, 'Patricia', 'Scott', 'Supervisor', '174 Cinn Ln', 60000, '1971-09-27', 'F', 130423454, 'Tiger Realm');
+insert into zooemployees values (543145276, 'Brenda', 'Myers', 'Supervisor', '233 Hollow Rd', 61000, '1962-05-31', 'F', 130423454, 'Shores Aquarium');
+insert into zooemployees values (187225055, 'Michael', 'Tejada', 'Supervisor', '531 Lone Rd', 62000, '1975-09-01', 'M', 130423454, 'Pelican Pier');
+insert into zooemployees values (397981967, 'David', 'Gullett', 'Supervisor', '479 Leg Road', 63000, '1979-01-29', 'M', 130423454, 'Tropic Treasures');
+insert into zooemployees values (405249752, 'Reginald', 'Phillips', 'Supervisor', '194 Park Ln', 64000, '1985-08-22', 'M', 130423454, 'Wild Way Trail');
+insert into zooemployees values (198204924, 'Jack', 'Arnold', 'Supervisor', '238 Pride Ave', 65000, '1983-05-10', 'M', 130423454, 'Petting Zoo');
+insert into zooemployees values (306369902, 'Robert', 'Bradley', 'Supervisor', '154 Neville St', 66000, '1974-01-24', 'M', 130423454, 'Africa');
+insert into zooemployees values (594494079, 'Sheila', 'Lane', 'Supervisor', '438 Badger Ln', 67000, '1975-11-13', 'F', 130423454, 'North America');
+insert into zooemployees values (660054663, 'Jennifer', 'Atencio', 'Supervisor', '403 Wood St', 68000, '1989-12-05', 'F', 130423454, 'South America');
+insert into zooemployees values (170725571, 'Allie', 'Owens', 'Supervisor', '347 Creek Rd', 69000, '1985-12-14', 'F', 130423454, 'Frogs');
 insert into zooemployees values (114327791, 'Jennifer', 'Doe', 'Supervisor', '710 Huntz Lane', 70000, '1970-12-22', 'F', 130423454, 'Forest Realm');
-insert into zooemployees values (216407536, 'Georgia', 'Murray', 'Supervisor', '2236 Blue Spruce Lane', 71000, '1976-05-19', 'F', 130423454, 'Monkeys');
+insert into zooemployees values (216407536, 'Georgia', 'Murray', 'Supervisor', '223 Blue Ln', 71000, '1976-05-19', 'F', 130423454, 'Monkeys');
 --
 -- Exhibits
 insert into exhibit values ('Tiger Realm', 'Temperate', 635052791);
@@ -273,7 +279,7 @@ insert into exhibit values ('Wild Way Trail', 'Temperate', 405249752);
 insert into exhibit values ('Petting Zoo', 'Temperate', 198204924);
 insert into exhibit values ('Africa', 'Arid', 306369902);
 insert into exhibit values ('North America', 'Temperate', 594494079);
-insert into exhibit values ('South America', 'Mediterranean', 660054663);
+insert into exhibit values ('South America', 'Coastal', 660054663);
 insert into exhibit values ('Frogs', 'Tropical', 170725571);
 insert into exhibit values ('Forest Realm', 'Temperate', 114327791);
 insert into exhibit values ('Monkeys', 'Tropical', 216407536);
@@ -297,14 +303,14 @@ insert into zooemployees values (251828867, 'Benjamin', 'Luong', 'Cashier', '231
 insert into zooemployees values (178269601, 'Gina', 'Clear', 'Cashier', '301 Coal St', 17400, '1990-08-27', 'F', 216407536, 'Monkeys');
 --
 --Zoo Baristas
-insert into zooemployees values (574127433, 'Monica', 'Kamp', 'Barista', '1709 Williams Mine Road', 20800, '1993-07-07', 'F', 187225055, 'Pelican Pier');
-insert into zooemployees values (255728306, 'Carl', 'Guerra', 'Barista', '4331 Hart Country Lane', 20900, '1988-10-16', 'M', 114327791, 'Forest Realm');
+insert into zooemployees values (574127433, 'Monica', 'Kamp', 'Barista', '170 Mine Rd', 20800, '1993-07-07', 'F', 187225055, 'Pelican Pier');
+insert into zooemployees values (255728306, 'Carl', 'Guerra', 'Barista', '433 Hart Ln', 20900, '1988-10-16', 'M', 114327791, 'Forest Realm');
 --
 -- Zoo Cooks
-insert into zooemployees values (427481397, 'Tania', 'Ramirez', 'Cook', '2043 Kelley Road', 22800, '1993-07-07', 'F', 397981967, 'Tropic Treasures');
-insert into zooemployees values (416860165, 'Terrence', 'Harrison', 'Cook', '35 Turkey Pen Lane', 22900, '1981-12-14', 'M', 397981967, 'Tropic Treasures');
-insert into zooemployees values (378213720, 'Avery', 'Foster', 'Cook', '691 Tennessee Avenue', 23000, '1994-10-18', 'M', 660054663, 'South America');
-insert into zooemployees values (256728014, 'Stephanie', 'Grant', 'Cook', '1513 Junior Avenue', 23100, '1991-10-21', 'F', 660054663, 'South America');
+insert into zooemployees values (427481397, 'Tania', 'Ramirez', 'Cook', '204 Kelley Rd', 22800, '1993-07-07', 'F', 397981967, 'Tropic Treasures');
+insert into zooemployees values (416860165, 'Terrence', 'Harrison', 'Cook', '35 Pen Ln', 22900, '1981-12-14', 'M', 397981967, 'Tropic Treasures');
+insert into zooemployees values (378213720, 'Avery', 'Foster', 'Cook', '691 Ten Ave', 23000, '1994-10-18', 'M', 660054663, 'South America');
+insert into zooemployees values (256728014, 'Stephanie', 'Grant', 'Cook', '151 Junior Ave', 23100, '1991-10-21', 'F', 660054663, 'South America');
 --
 -- Caretakers
 insert into zooemployees values (622347022, 'Annie', 'Allen', 'Caretaker', '797 Heritage Rd', 35000, '1986-11-05', 'F', 635052791, 'Tiger Realm');
@@ -321,18 +327,18 @@ insert into zooemployees values (265036954, 'Kathy', 'Stewart', 'Caretaker', '48
 insert into zooemployees values (103010924, 'Marjorie', 'Castaneda', 'Caretaker', '182 Anmoore Rd', 36100, '1985-07-22', 'F', 216407536, 'Monkeys');
 --
 -- Vets
-insert into zooemployees values (571707179, 'Peggy', 'Baker', 'Vet', '1696 Black Oak Hollow Road', 50000, '1973-10-28', 'F', 635052791, 'Tiger Realm');
-insert into zooemployees values (166701386, 'Silas', 'Foulk', 'Vet', '4373 Saint James Drive', 51000, '1976-04-22', 'M', 543145276, 'Shores Aquarium');
-insert into zooemployees values (460085712, 'Doris', 'Patterson', 'Vet', '697 South Street', 52000, '1979-07-13', 'F', 187225055, 'Pelican Pier');
-insert into zooemployees values (479360155, 'Steven', 'Davis', 'Vet', '4355 Woodland Drive', 53000, '1983-03-06', 'M', 397981967, 'Tropic Treasures');
-insert into zooemployees values (542100923, 'Veronica', 'Morales', 'Vet', '1655 Heron Way', 54000, '1984-08-17', 'F', 405249752, 'Wild Way Trail');
-insert into zooemployees values (457903419, 'Connie', 'Landis', 'Vet', '3637 Colonial Drive', 55000, '1971-05-29', 'F', 198204924, 'Petting Zoo');
-insert into zooemployees values (265755073, 'John', 'Joyner', 'Vet', '804 Woodside Circle', 56000, '1982-04-25', 'M', 306369902, 'Africa');
-insert into zooemployees values (241380147, 'Michael', 'Rodriguez', 'Vet', '737 Keyser Ridge Road', 57000, '1975-11-13', 'M', 594494079, 'North America');
-insert into zooemployees values (765189816, 'William', 'Richie', 'Vet', '3888 Crowfield Road', 58000, '1974-10-05', 'M', 660054663, 'South America');
-insert into zooemployees values (681108756, 'Jessica', 'Gonzalez', 'Vet', '988 Concord Street', 59000, '1991-07-11', 'F', 170725571, 'Frogs');
-insert into zooemployees values (126122218, 'Angela', 'Rice', 'Vet', '4760 Valley View Drive', 60000, '1987-12-11', 'F', 114327791, 'Forest Realm');
-insert into zooemployees values (168542187, 'Nathan', 'Jones', 'Vet', '2631 Browning Lane', 61000, '1980-12-29', 'M', 216407536, 'Monkeys');
+insert into zooemployees values (571707179, 'Peggy', 'Baker', 'Vet', '169 Oak Rd', 50000, '1973-10-28', 'F', 635052791, 'Tiger Realm');
+insert into zooemployees values (166701386, 'Silas', 'Foulk', 'Vet', '437 James Dr', 51000, '1976-04-22', 'M', 543145276, 'Shores Aquarium');
+insert into zooemployees values (460085712, 'Doris', 'Patterson', 'Vet', '697 South St', 52000, '1979-07-13', 'F', 187225055, 'Pelican Pier');
+insert into zooemployees values (479360155, 'Steven', 'Davis', 'Vet', '435 Woodland Dr', 53000, '1983-03-06', 'M', 397981967, 'Tropic Treasures');
+insert into zooemployees values (542100923, 'Veronica', 'Morales', 'Vet', '165 Heron Way', 54000, '1984-08-17', 'F', 405249752, 'Wild Way Trail');
+insert into zooemployees values (457903419, 'Connie', 'Landis', 'Vet', '363 Course Dr', 55000, '1971-05-29', 'F', 198204924, 'Petting Zoo');
+insert into zooemployees values (265755073, 'John', 'Joyner', 'Vet', '804 Wood St', 56000, '1982-04-25', 'M', 306369902, 'Africa');
+insert into zooemployees values (241380147, 'Michael', 'Rodriguez', 'Vet', '737 Ridge Rd', 57000, '1975-11-13', 'M', 594494079, 'North America');
+insert into zooemployees values (765189816, 'William', 'Richie', 'Vet', '388 Crow Rd', 58000, '1974-10-05', 'M', 660054663, 'South America');
+insert into zooemployees values (681108756, 'Jessica', 'Gonzalez', 'Vet', '988 Concord St', 59000, '1991-07-11', 'F', 170725571, 'Frogs');
+insert into zooemployees values (126122218, 'Angela', 'Rice', 'Vet', '476 Valley Dr', 60000, '1987-12-11', 'F', 114327791, 'Forest Realm');
+insert into zooemployees values (168542187, 'Nathan', 'Jones', 'Vet', '263 Brown Ln', 61000, '1980-12-29', 'M', 216407536, 'Monkeys');
 --
 -- Shops
 insert into shop values (10, 'Coffee Station', 'Pelican Pier');
@@ -617,20 +623,24 @@ where E.egender = 'M' and
 -- INSERT/DELETE/UPDATE STATEMENTS
 -- -----------------------------------------------------
 --
+-- Testing K1
+insert into exhibit values ('Petting Zoo','Temperate', 114327791);
 -- Testing ZC1 violate
-insert into zooemployees values (144423650, 'Andy', 'Carl', 'Person', '4960 Farland Street', 8000, '1970-04-15', 'M', NULL, 'Bugs');
+insert into zooemployees values (144423650, 'Andy', 'Carl', 'Person', '496 Land St', 8000, '1970-04-15', 'M', NULL, 'Bugs');
 -- Testing ZC2 violate
-insert into zooemployees values (133333651, 'Ju-lee', 'Carl', 'Supervisor', '4960 Farland Street', 80000, '1970-04-15', 'R', NULL, 'Bugs');
+insert into zooemployees values (133333651, 'Ju-lee', 'Carl', 'Supervisor', '460 Land St', 80000, '1970-04-15', 'R', NULL, 'Bugs');
 -- Testing ZC3 violate
-insert into zooemployees values (130423650, 'Beth', 'Carl', 'Supervisor', '4960 Farland Street', 8000, '1970-04-15', 'M', NULL, 'Bugs');
+insert into zooemployees values (130423650, 'Beth', 'Carl', 'Supervisor', '960 Land St', 80000, '1970-04-15', 'M', NULL, 'Bugs');
 -- Testing ZC4 violate
 insert into exhibit values ('Petting Zoo', 'Cold', 198204924);
 -- Testing ZC5 violate
 insert into animal values (100, 'Tiger', 5, 'R', 622347022, 1030, 'Tiger Realm');
 -- Testing ZC6 violate
-insert into animal values (479, 'Gorilla', 5, 'F', 103010924, 100, 'Monkeys');
+insert into animal values (479, 'Gorilla', 2, 'F', 103010924, 100, 'Monkeys');
 -- Testing FK1 
 delete from zooemployees WHERE empssn = 405249752;
+-- Testing FK1.1 should violate the parent key constraint
+insert into zooemployees values (144423650, 'Andy', 'Carl', 'Barista', '496 Land St', 8000, '1970-04-15', 'M', 000000000, 'Bugs');
 -- Testing FK2 
 delete from zooemployees WHERE exhibitname = 'Shores Aquarium';
 -- Testing FK3 
@@ -642,7 +652,7 @@ delete from animal where empssn = 595061909;
 -- testing FK6 
 delete from animal where exhibitname = 'Pelican Pier';
 -- Testing FK7 
-delete from event where exhibitname = 'Petting Zoo';
+delete from event where exhibitname = 'Forest Real';
 -- Testing FK8 
 delete from shopproducts where shopid = 12;
 -- Testing FK9 
